2ebb2f93539788345cca666612463a107407e270
Yanis BERTHEBAUD <yanis.berthebaud@isen.yncrea.fr>
2020-11-18 10:06:28 +0100

    écart type fini

--- TPC3/Exo 3 2e part/Source.c
+++ TPC3/Exo 3 2e part/Source.c
@@ -16,7 +16,7 @@ int main() {
 	int nbA = 0;
 	float petite = 20;
 	float grande = 0;
-
+	float p = 0;
 	for (int k = 1; k <= NBMAXNOTES; k++) {
 		note[k - 1] = -2;
 	}
@@ -24,17 +24,17 @@ int main() {
 	while ((i <= 30) && (question != 'O')) {
 
 		printf("entrez la note no %d\n", i);
-		scanf_s("%f", &note[i-1]);
-		if ((note[i-1] <= 20) && (note[i-1] >= 0)) {
-			
+		scanf_s("%f", &p);
+		if ((p <= 20) && (p >= 0)) {
+			note[i - 1] = p;
 			numerateur += note[i - 1];
 			denominateur++;
-			
-			if (note[i - 1] < petite) {
-				petite = note[i - 1];
+
+			if (p < petite) {
+				petite = p;
 			}
 			if (note[i - 1] > grande) {
-				grande = note[i - 1];
+				grande = p;
 			}
 			i++;
 		}
@@ -65,10 +65,13 @@ int main() {
 	for (int j = 0; j < NBMAXNOTES; j++) {
 		if ((note[j] != -1) && (note[j] != -2)) {
 			somme += carre(note[j] - moyenne);
-
+			
 		}
-		et = (float)sqrt((1 / (i - nbA - 1)) * somme);
+
 	}
+	et = (float)pow(((float)(1 / (i - nbA - (float)2)) * somme), 0.5);
 	printf("la moyenne est de classe est de %f ", moyenne);
 	printf("il y a %d note valides, il y a %d absence, la plus petite note est %f, la plus grande note est %f et l'écart type est %f\n", i - 1 - nbA, nbA, petite, grande, et);
+	printf("%f %f\n", 1 / (i - nbA - (float)1), somme);
+	printf("%d %d\n", i, nbA);
 }
